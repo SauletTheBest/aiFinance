c1507cd2cd5cb8c07341301218d9612b950a20c9
SauletTheBest <231003@astanait.edu.kz>
2026-06-06 21:51:13 +0500

    hotfix: debugg

--- pkg/google/verifier.go
+++ pkg/google/verifier.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"google.golang.org/api/idtoken"
+    "github.com/golang-jwt/jwt/v5"
 )
 
 type Verifier struct {
@@ -16,6 +17,15 @@ func NewVerifier(clientID string) *Verifier {
 }
 
 func (v *Verifier) VerifyToken(ctx context.Context, idToken string) (string, string, error) {
+	//Print aud claim
+	parsedToken, _, err := new(jwt.Parser).ParseUnverified(idToken, jwt.MapClaims{})
+	if err == nil {
+		if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok {
+			fmt.Printf("\n[DEBUG] Incoming Google Token 'aud' claim is: %v\n", claims["aud"])
+			fmt.Printf("[DEBUG] Backend expected 'aud' claim is: %s\n\n", v.clientID)
+		}
+	}
+	//
 	payload, err := idtoken.Validate(ctx, idToken, v.clientID)
 	if err != nil {
 		return "", "", fmt.Errorf("token validation failed: %w", err)
